9c379af26cefacec3b08706063aa98429fdae656
Glev Elenev <g.elenev@gmail.com>
2023-04-25 05:32:50 +0300

    feat: add layouts for en and ru

---
+++ src/js/en.js
@@ -0,0 +1,324 @@
+const en = [
+  {
+    symbol: '`',
+    shift: '~',
+    code: 'Backquote',
+  },
+  {
+    symbol: '1',
+    shift: '!',
+    code: 'Digit1',
+  },
+  {
+    symbol: '2',
+    shift: '@',
+    code: 'Digit2',
+  },
+  {
+    symbol: '3',
+    shift: '#',
+    code: 'Digit3',
+  },
+  {
+    symbol: '4',
+    shift: '$',
+    code: 'Digit4',
+  },
+  {
+    symbol: '5',
+    shift: '%',
+    code: 'Digit5',
+  },
+  {
+    symbol: '6',
+    shift: '^',
+    code: 'Digit6',
+  },
+  {
+    symbol: '7',
+    shift: '&',
+    code: 'Digit7',
+  },
+  {
+    symbol: '8',
+    shift: '*',
+    code: 'Digit8',
+  },
+  {
+    symbol: '9',
+    shift: '(',
+    code: 'Digit9',
+  },
+  {
+    symbol: '0',
+    shift: ')',
+    code: 'Digit0',
+  },
+  {
+    symbol: '-',
+    shift: '_',
+    code: 'Minus',
+  },
+  {
+    symbol: '=',
+    shift: '+',
+    code: 'Equal',
+  },
+  {
+    symbol: '🠔 Backspace',
+    shift: '🠔 Backspace',
+    code: 'Backspace',
+  },
+  {
+    symbol: 'Tab',
+    shift: 'Tab',
+    code: 'Tab',
+  },
+  {
+    symbol: 'q',
+    shift: 'Q',
+    code: 'KeyQ',
+  },
+  {
+    symbol: 'w',
+    shift: 'W',
+    code: 'KeyW',
+  },
+  {
+    symbol: 'e',
+    shift: 'E',
+    code: 'KeyE',
+  },
+  {
+    symbol: 'r',
+    shift: 'R',
+    code: 'KeyR',
+  },
+  {
+    symbol: 't',
+    shift: 'T',
+    code: 'KeyT',
+  },
+  {
+    symbol: 'y',
+    shift: 'Y',
+    code: 'KeyY',
+  },
+  {
+    symbol: 'u',
+    shift: 'U',
+    code: 'KeyU',
+  },
+  {
+    symbol: 'i',
+    shift: 'I',
+    code: 'KeyI',
+  },
+  {
+    symbol: 'o',
+    shift: 'O',
+    code: 'KeyO',
+  },
+  {
+    symbol: 'p',
+    shift: 'P',
+    code: 'KeyP',
+  },
+  {
+    symbol: '[',
+    shift: '{',
+    code: 'BracketLeft',
+  },
+  {
+    symbol: ']',
+    shift: '}',
+    code: 'BracketRight',
+  },
+  {
+    symbol: '\\',
+    shift: '|',
+    code: 'Backslash',
+  },
+  {
+    symbol: 'Caps Lock',
+    shift: 'Caps Lock',
+    code: 'CapsLock',
+  },
+  {
+    symbol: 'a',
+    shift: 'A',
+    code: 'KeyA',
+  },
+  {
+    symbol: 's',
+    shift: 'S',
+    code: 'KeyS',
+  },
+  {
+    symbol: 'd',
+    shift: 'D',
+    code: 'KeyD',
+  },
+  {
+    symbol: 'f',
+    shift: 'F',
+    code: 'KeyF',
+  },
+  {
+    symbol: 'g',
+    shift: 'G',
+    code: 'KeyG',
+  },
+  {
+    symbol: 'h',
+    shift: 'H',
+    code: 'KeyH',
+  },
+  {
+    symbol: 'j',
+    shift: 'J',
+    code: 'KeyJ',
+  },
+  {
+    symbol: 'k',
+    shift: 'K',
+    code: 'KeyK',
+  },
+  {
+    symbol: 'l',
+    shift: 'L',
+    code: 'KeyL',
+  },
+  {
+    symbol: ';',
+    shift: ':',
+    code: 'Semicolon',
+  },
+  {
+    symbol: "'",
+    shift: '"',
+    code: 'Quote',
+  },
+  {
+    symbol: 'Enter',
+    shift: 'Enter',
+    code: 'Enter',
+  },
+  {
+    symbol: 'Shift',
+    shift: 'Shift',
+    code: 'ShiftLeft',
+  },
+  {
+    symbol: 'z',
+    shift: 'Z',
+    code: 'KeyZ',
+  },
+  {
+    symbol: 'x',
+    shift: 'X',
+    code: 'KeyX',
+  },
+  {
+    symbol: 'c',
+    shift: 'C',
+    code: 'KeyC',
+  },
+  {
+    symbol: 'v',
+    shift: 'V',
+    code: 'KeyV',
+  },
+  {
+    symbol: 'b',
+    shift: 'B',
+    code: 'KeyB',
+  },
+  {
+    symbol: 'n',
+    shift: 'N',
+    code: 'KeyN',
+  },
+  {
+    symbol: 'm',
+    shift: 'M',
+    code: 'KeyM',
+  },
+  {
+    symbol: ',',
+    shift: '<',
+    code: 'Comma',
+  },
+  {
+    symbol: '.',
+    shift: '>',
+    code: 'Period',
+  },
+  {
+    symbol: '/',
+    shift: '?',
+    code: 'Slash',
+  },
+  {
+    symbol: '🠕',
+    shift: '🠕',
+    code: 'ArrowUp',
+  },
+  {
+    symbol: 'Shift',
+    shift: 'Shift',
+    code: 'ShiftRight',
+  },
+  {
+    symbol: 'Ctrl',
+    shift: 'Ctrl',
+    code: 'ControlLeft',
+  },
+  {
+    symbol: 'En',
+    shift: 'En',
+    code: 'lang',
+  },
+  {
+    symbol: 'Alt',
+    shift: 'Alt',
+    code: 'AltLeft',
+  },
+  {
+    symbol: ' ',
+    shift: ' ',
+    code: 'Space',
+  },
+  {
+    symbol: 'Alt',
+    shift: 'Alt',
+    code: 'AltRight',
+  },
+  {
+    symbol: 'Del',
+    shift: 'Del',
+    code: 'Delete',
+  },
+  {
+    symbol: '🠔',
+    shift: '🠔',
+    code: 'ArrowLeft',
+  },
+  {
+    symbol: '🠗',
+    shift: '🠗',
+    code: 'ArrowDown',
+  },
+  {
+    symbol: '🠖',
+    shift: '🠖',
+    code: 'ArrowRight',
+  },
+  {
+    symbol: 'Ctrl',
+    shift: 'Ctrl',
+    code: 'ControlRight',
+  },
+];
+
+export default en;
